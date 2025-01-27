5eb187e51df221dc3793694c060bb40534e6111c
root <root@localhost>
2025-01-27 04:37:32 -0300

    test

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*document.querySelector('.button-1').addEventListener('click', () => {
+  alert('¡Descarga iniciada!');
+});*/
+
+/*document.querySelector('.button-2').addEventListener('click', () => {
+  alert('Más información próximamente.');
+});*/
